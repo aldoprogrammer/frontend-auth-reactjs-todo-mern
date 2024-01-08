3c30a063830c4207ee0e28cd6e60a960072209c0
aldoprogrammer <aldobesma@gmail.com>
2024-01-08 11:11:32 +0700

    succes user

--- src/Register.js
+++ src/Register.js
@@ -4,30 +4,38 @@ import axios from 'axios'
 const Register = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('');
+
     function registerUser(e) {
         e.preventDefault();
-        const data = {email, password}
-        axios.post('http://localhost:5000/register', {
-            data: data
-        })
+        const data = { email, password };
+    
+        axios.post('http://localhost:4000/register', data, { withCredentials: true })
+            .then(response => {
+                console.log("Registration successful:", response?.data);
+            })
+            .catch(error => {
+                console.error("Registration failed:", error?.response?.data || error.message);
+            });
     }
+    
 
-  return (
-    <form onSubmit={e => registerUser(e)}>
-        <input type='email' 
-            placeholder='email'
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+    return (
+        <form onSubmit={e => registerUser(e)}>
+            <input type='email'
+                placeholder='email'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
             /><br />
-        <input type='password' 
-            placeholder='password'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            <input type='password'
+                placeholder='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
             /><br />
-        <button type='submit'>
-            Register</button>
-    </form>
-  )
+            <button type='submit'>
+                Register
+            </button>
+        </form>
+    )
 }
 
-export default Register
+export default Register;
